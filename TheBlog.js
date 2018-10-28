f339259032c4a4d2dc15d68b05b4fbcaeb0a8abf
JanS <jsramek@hotmail.cz>
2018-10-28 12:35:30 +0100

    Firebase linked

--- TheBlog.js
+++ TheBlog.js
@@ -14,4 +14,11 @@ $(`form`).on(`submit`, event => {
         else { alert(`Your Post is empty`) }
     }
     else { alert(`Your Tittle is empty`) }
+    let path = "posts/1";
+    let dataToSave = {
+        title: "My first saved blog post",
+        text: "Some hilarious content, which proves how awesome I am."
+};
+fb.ref(path).set(dataToSave);
 });
+
