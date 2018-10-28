769b296d9923f0057a9d61179e04f41579f75cc6
JanS <jsramek@hotmail.cz>
2018-10-28 13:26:59 +0100

    Variabilni posty na Firebase

--- TheBlog.js
+++ TheBlog.js
@@ -1,24 +1,32 @@
+let base = 0;
+let counter = 0;
+
 $(`form`).on(`submit`, event => {
     event.preventDefault();
     let title = $(`input`).val();
     let post = $(`textarea`).val();
     let tittleCount = title.length;
     let postCount = post.length;
-    if (tittleCount > 0) {
+        if (tittleCount > 0) {
         if (postCount > 0) {
+            counter = base + 1;
+            let path = `posts/${counter}`;
+            base = counter;
+            let dataToSave = {
+                title: `${title}`,
+                text: `${post}`,
+                postId: `${counter}`
+            };
+            fb.ref(path).set(dataToSave);
             $(`.postSec`).prepend(`<h4 class="title">${title}</h4>
             <div class="post">${post}</div>`);
             $(`input[name=inputTitle]`).val(``);
             $(`textarea`).val(``);
+            
         }
         else { alert(`Your Post is empty`) }
     }
     else { alert(`Your Tittle is empty`) }
-    let path = "posts/1";
-    let dataToSave = {
-        title: "My first saved blog post",
-        text: "Some hilarious content, which proves how awesome I am."
-};
-fb.ref(path).set(dataToSave);
+
 });
 
