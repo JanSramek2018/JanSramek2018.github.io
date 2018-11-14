dee0a9af98fdfe4d3d612080d0179bb3e3829b70
JanS <jsramek@hotmail.cz>
2018-11-15 00:31:08 +0100

    Canvas_01

---
+++ Canvas.js
@@ -0,0 +1,14 @@
+let base = 0
+let counter = 0
+
+$(`#Increase`).on(`click`, event => {
+    counter = base + 1;
+    $(`#counterNumber`).text(`${counter}`);
+    base = counter;
+})
+
+$(`#Decrease`).on(`click`, event => {
+    counter = base - 1;
+    $(`#counterNumber`).text(`${counter}`);
+    base = counter;
+})
