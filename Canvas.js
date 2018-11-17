38bf749b04ed0ae7b334b335acef0d55cae0f08f
JanS <jsramek@hotmail.cz>
2018-11-17 16:34:22 +0100

    Pokus o sestaveni funkce

--- Canvas.js
+++ Canvas.js
@@ -11,21 +11,21 @@ let t = 0;
 
 
 
-n = $(`#elementsNumber`).val()*1;
+n = $(`#elementsNumber`).val() * 1;
 console.log(`n=${n}`);
-s = $(`#elementsSize`).val()*1;
+s = $(`#elementsSize`).val() * 1;
 console.log(`s=${s}`);
-x = $(`#elementsX`).val()*1;
+x = $(`#elementsX`).val() * 1;
 console.log(`x=${x}`);
-y = $(`#elementsY`).val()*1;
+y = $(`#elementsY`).val() * 1;
 console.log(`y=${y}`);
-offset = $(`#offset`).val()*1;
+offset = $(`#offset`).val() * 1;
 console.log(`offset=${offset}`);
-r = $(`#elementsR`).val()*1;
+r = $(`#elementsR`).val() * 1;
 console.log(`r=${r}`);
-g = $(`#elementsG`).val()*1;
+g = $(`#elementsG`).val() * 1;
 console.log(`g=${g}`);
-b = $(`#elementsB`).val()*1;
+b = $(`#elementsB`).val() * 1;
 console.log(`b=${b}`);
 t = $(`#elementsT`).val() / 100;
 console.log(`t=${t}`);
@@ -41,47 +41,57 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
-  }
+}
+
+function drawCanvas ()
+
+
+
+
+
+
+
+
 
 $(`#elementsSize`).change(function () {
-    s = $(`#elementsSize`).val()*1;
-    offsetSq = $(`#offset`).val()*1; 
+    s = $(`#elementsSize`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
     console.log(`s=${s}`);
-        
+
     ctx.clearRect(0, 0, w, h);
     ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
     ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
     if (elementType === `square`) {
-             
+
         for (var i = 0; i < n; i = i + 1) {
-        ctx.fillRect(x+offsetSq, y+offsetSq, s, s);
-        offsetSq = (offsetSq+offset);
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
         }
     }
     if (elementType === `rectangular`) {
-             
+
         for (var i = 0; i < n; i = i + 1) {
-        ctx.fillRect(x+offsetSq, y+offsetSq, 2*s, s);
-        offsetSq = (offsetSq+offset);
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
         }
     }
     if (elementType === `triangle`) {
 
         ctx.beginPath();
         for (var i = offsetSq; i <= 200; i = i + n) {
-        ctx.moveTo(x+i, y+i);
-        ctx.lineTo(x+s/4*3+i, y+s/4*3+i);
-        ctx.lineTo(x-s/4*3+i, y+s/4*3+i);
-        ctx.lineTo(x+i, y+i);
-        ctx.stroke();
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
         }
     }
     if (elementType === `circle`) {
         ctx.beginPath();
         for (var i = 0; i <= n; i = i + 1) {
-        ctx.arc(x, y, s+offsetSq, 0, 2 * Math.PI);
-        ctx.stroke();
-        offsetSq = (offsetSq+offset)
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
         }
     }
     if (elementType === `lines`) {
@@ -90,39 +100,488 @@ $(`#elementsSize`).change(function () {
             var yl = i * h / (n - 1);
             drawLine(0, yl, xl, h);
             drawLine(xl, 0, w, yl);
-          }
-          
+        }
+
     }
+});
 
+$(`#elementsNumber`).change(function () {
+    n = $(`#elementsNumber`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${n}`);
 
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
 
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
 
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
 
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
 
-
-
-
-
-
-
-
-
-
-})
-
+    }
+});
 
 $(`#elementsType`).change(function () {
-    elementType = $(`#elementsType`).val();
-    console.log(elementType);
+    elementType = $(`#elementsType`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${elementType}`);
 
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
 
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
 
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
 
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
 
-
-
-
+    }
 });
+
+$(`#elementsX`).change(function () {
+    x = $(`#elementsX`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${x}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+$(`#elementsY`).change(function () {
+    y = $(`#elementsY`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${y}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+$(`#elementsR`).change(function () {
+    r = $(`#elementsR`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${r}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+$(`#elementsG`).change(function () {
+    g = $(`#elementsG`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${g}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+$(`#elementsB`).change(function () {
+    b = $(`#elementsB`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${b}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+$(`#elementsT`).change(function () {
+    t = $(`#elementsT`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${t}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+$(`#offset`).change(function () {
+    offset = $(`#offset`).val() * 1;
+    offsetSq = $(`#offset`).val() * 1;
+    console.log(`s=${offset}`);
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `rgba(${r},${g},${b},${t})`;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${t})`;
+    if (elementType === `square`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `rectangular`) {
+
+        for (var i = 0; i < n; i = i + 1) {
+            ctx.fillRect(x + offsetSq, y + offsetSq, 2 * s, s);
+            offsetSq = (offsetSq + offset);
+        }
+    }
+    if (elementType === `triangle`) {
+
+        ctx.beginPath();
+        for (var i = offsetSq; i <= 200; i = i + n) {
+            ctx.moveTo(x + i, y + i);
+            ctx.lineTo(x + s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x - s / 4 * 3 + i, y + s / 4 * 3 + i);
+            ctx.lineTo(x + i, y + i);
+            ctx.stroke();
+        }
+    }
+    if (elementType === `circle`) {
+        ctx.beginPath();
+        for (var i = 0; i <= n; i = i + 1) {
+            ctx.arc(x, y, s + offsetSq, 0, 2 * Math.PI);
+            ctx.stroke();
+            offsetSq = (offsetSq + offset)
+        }
+    }
+    if (elementType === `lines`) {
+        for (var i = 0; i < n; i = i + 1) {
+            var xl = i * w / (n - 1);
+            var yl = i * h / (n - 1);
+            drawLine(0, yl, xl, h);
+            drawLine(xl, 0, w, yl);
+        }
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
