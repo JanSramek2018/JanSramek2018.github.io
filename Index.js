87eacfabf626e05ee50f497758769b95ef5e609f
JanS <jsramek@hotmail.cz>
2018-11-22 19:03:45 +0100

    Index page - post field design change

--- Index.js
+++ Index.js
@@ -1,20 +1,18 @@
 
 // Nacteni poctu ulozenych prispevku
-let base = 0;
-let postsDB;
 
 fb.ref("posts").once('value').then(data => {
     let savedPosts = data.val();
-    base = savedPosts.length;
+    let postsDB;
     postsDB = savedPosts;
 
-    // Nacteni ulozenych prispevku
+    
 
     postsDB.forEach(post => {
         $(`.postSec`).prepend(`<div id="id${post[`postId`]}">
                     <h4 class="tittle">${post[`tittle`]}</h4>
-                    <div class="post">${post[`text`]}</div>
+                    <div class="postMainPage">${post[`text`]}</div>
         </div>`);
+    })
 })
-})
-;
+// Nacteni ulozenych prispevku
