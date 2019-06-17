c326329a06db1fe886e0b27ea7d9a02278076546
JanS <jsramek@hotmail.cz>
2019-06-17 14:11:59 +0200

    Calculator is fully working

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ const keys = calculator.querySelector('.calculatorKeys');
 const display = document.querySelector('#displayNumber');
 var decimalKeyPressed = 0;
 
+
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
@@ -14,11 +15,9 @@ keys.addEventListener('click', e => {
         if (!action) {
             console.log('number key was pressed');
 
-
-
             if (displayedNumber === '0' ||
                 previousKeyType === 'operator' ||
-                previousKeyType === 'equal'
+                previousKeyType === 'calculate'
             ) {
                 display.textContent = keyContent;
                 calculator.dataset.previousKeyType = 'numberKey';
@@ -34,30 +33,89 @@ keys.addEventListener('click', e => {
             action === 'add'
         ) {
             console.log('operator key was pressed');
-            calculator.dataset.previousKeyType = 'operator';
-            calculator.dataset.firstNumber = displayedNumber;
-            console.log('Stored number is ', displayedNumber);
+            decimalKeyPressed = 0;
+            console.log(`Is decimal active? `, decimalKeyPressed);
+            
+            
+            
+
+            const firstNumber = calculator.dataset.firstNumber;
+            const secondNumber = displayedNumber;
+            const operator = calculator.dataset.operator;
+
+            function calculate(firstNumber, operator, secondNumber) {
+                let result;
+                if (operator === 'add') {
+                    result = parseFloat(firstNumber) + parseFloat(secondNumber);
+                }
+                else if (operator === 'subtract') {
+                    result = parseFloat(firstNumber) - parseFloat(secondNumber);
+                }
+
+                else if (operator === 'divide') {
+                    result = parseFloat(firstNumber) / parseFloat(secondNumber);
+                }
+
+                else if (operator === 'multiply') {
+                    result = parseFloat(firstNumber) * parseFloat(secondNumber);
+                }
+                return result;
+            }
+
+            if (firstNumber &&
+                operator && 
+                previousKeyType !== `operator`&& 
+                previousKeyType !== `calculate`
+                ) {
+                const calculationNumber = calculate(firstNumber, operator, secondNumber);
+                display.textContent = calculationNumber;
+                calculator.dataset.firstNumber = calculationNumber;
+                console.log(`AAAA`);
+              }
+            else {
+                calculator.dataset.firstNumber = displayedNumber
+                console.log(`BBBB`);
+            }
             calculator.dataset.operator = action;
+            calculator.dataset.previousKeyType = 'operator';
+            console.log('Stored number is ', displayedNumber);
         }
         if (action === `decimal`) {
             console.log('decimal key was pressed');
-            console.log(`Was decimal key pressed? `, decimalKeyPressed);
-            if (decimalKeyPressed = 0) {
+            console.log(`Is decimal active? `, decimalKeyPressed);
+            if (decimalKeyPressed === 0) {
 
-            display.textContent = displayedNumber + '.';
-            decimalKeyPressed = 1;
-        }
-            console.log(`Was decimal key pressed? `, decimalKeyPressed);
+                if (previousKeyType === `operator` ||
+                previousKeyType === `calculate`
+                ) {
+                    display.textContent = `0` + '.';
+
+                    decimalKeyPressed = 1;}
+                else {
+                    display.textContent = displayedNumber + '.';
+                    decimalKeyPressed = 1;}
+            calculator.dataset.previousKeyType = 'decimal';
+            }           
+            console.log(`Is decimal active? `, decimalKeyPressed);
         }
 
         if (action === 'delete') {
             console.log('delete key was pressed');
             display.textContent = '0';
+            decimalKeyPressed = 0;
+            console.log(`Is decimal active? `, decimalKeyPressed);
+            calculator.dataset.firstNumber = ``;
+            calculator.dataset.secondNumber = ``;
+            calculator.dataset.previousSecondNumber = ``;
+            calculator.dataset.operator = ``;
+            calculator.dataset.previousKeyType = 'delete';
         }
         if (action === 'calculate') {
-            console.log('equal key was pressed')
-            const firstNumber = calculator.dataset.firstNumber;
-            const secondNumber = displayedNumber;
+            console.log('equal key was pressed');
+            decimalKeyPressed = 0;
+            console.log(`Is decimal active? `, decimalKeyPressed);
+            let firstNumber = calculator.dataset.firstNumber;
+            let secondNumber = displayedNumber;
             const operator = calculator.dataset.operator;
             function calculate(firstNumber, operator, secondNumber) {
                 let result;
@@ -77,12 +135,24 @@ keys.addEventListener('click', e => {
                 }
                 return result;
             }
-            display.textContent = calculate(firstNumber, operator, secondNumber);
-            console.log('Result is ', calculate(firstNumber, operator, secondNumber));
+            if (firstNumber) {
+                if (previousKeyType === `calculate`) {
+                    firstNumber = displayedNumber;
+                    secondNumber = calculator.dataset.previousSecondNumber
+                }
+                display.textContent = calculate(firstNumber, operator, secondNumber);
+                console.log('Result is ', calculate(firstNumber, operator, secondNumber));
+            }
+            else {
+                display.textContent = `0`;
+            }
+            calculator.dataset.previousSecondNumber = secondNumber;
+            calculator.dataset.previousKeyType = 'calculate';
         }
+    }
 
 
 
 
     }
-})
+)
